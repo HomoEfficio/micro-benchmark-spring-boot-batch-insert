90965a5cb42c3a07f9b2eaad8886d247feac9e76
Oh Myung Woon <omwomw@sk.com>
2020-01-25 00:23:17 +0900

    Initial Commit

---
+++ src/main/java/io/homo_efficio/springboot/micro_benchmark/batch_insert/MicroBenchmarkBatchInsertApplication.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.springboot.micro_benchmark.batch_insert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBenchmarkBatchInsertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBenchmarkBatchInsertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/homo_efficio/springboot/micro_benchmark/batch_insert/MicroBenchmarkBatchInsertApplicationTests.java
@@ -0,0 +1,13 @@
+package io.homo_efficio.springboot.micro_benchmark.batch_insert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBenchmarkBatchInsertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
